7575e48bbc7fb5979e018e37045c2e66ed1261fa
Raimon Esteve <raimonesteve@gmail.com>
2021-12-24 14:46:16 +0100

    Adapt category sequence to code and suffix_code Since issue9512 #048626

--- tests/test_product_sequence.py
+++ tests/test_product_sequence.py
@@ -2,7 +2,6 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-from decimal import Decimal
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.pool import Pool
@@ -27,75 +26,94 @@ class ProductSequenceTestCase(ModuleTestCase):
                     ('name', '=', 'Unit'),
                     ], limit=1)
 
-        sequence_type, = SequenceType.search([(
-                    'name', '=', 'Product Category',
+        product_sequence_type, = SequenceType.search([(
+                    'name', '=', 'Product',
                     )], limit=1)
-        category_sequence, = Sequence.create([{
-                    'name': 'Category',
-                    'sequence_type': sequence_type,
-                    'prefix': 'CAT',
-                    }])
-        sequence_type, = SequenceType.search([(
+        variant_sequence_type, = SequenceType.search([(
                     'name', '=', 'Variant',
                     )], limit=1)
-        product_sequence, = Sequence.create([{
+        cat_pt_sequence, cat_pp_sequence, pt_sequence = Sequence.create([{
+                    'name': 'Category Product',
+                    'sequence_type': product_sequence_type,
+                    'prefix': 'CAT-PT',
+                    }, {
+                    'name': 'Category Variant',
+                    'sequence_type': variant_sequence_type,
+                    'prefix': 'CAT-PP',
+                    }, {
                     'name': 'Product',
-                    'sequence_type': sequence_type,
-                    'prefix': 'PROD',
+                    'sequence_type': product_sequence_type,
+                    'prefix': 'PT',
                     }])
-
-        category1, = Category.create([{
-                    'name': 'Category 1',
+        cat1, cat2, = Category.create([{
+                    'name': 'Category PP-PT',
                     'category_sequence': True,
-                    'product_sequence': category_sequence,
+                    'product_sequence': cat_pp_sequence,
+                    'template_sequence': cat_pt_sequence,
+                    }, {
+                    'name': 'Category PT',
+                    'category_sequence': True,
+                    'template_sequence': cat_pt_sequence,
                     }])
-        self.assertTrue(category1.id)
+        self.assertTrue(cat1.id)
+        self.assertTrue(cat2.id)
 
         pt1, pt2 = Template.create([{
                     'name': 'P1',
                     'type': 'goods',
-                    'list_price': Decimal(20),
                     'default_uom': unit.id,
                     'products': [('create', [{
-                                    'suffix_code': 'PT1',
+                                    'suffix_code': 'F1',
                                     }])]
                     }, {
                     'name': 'P2',
                     'type': 'goods',
-                    'list_price': Decimal(20),
                     'default_uom': unit.id,
                     'products': [('create', [{
                                     'description': 'P2',
                                     }])]
                     }])
-        self.assertEqual(pt1.products[0].code, 'PT1')
+        self.assertEqual(pt1.products[0].code, 'F1')
         self.assertEqual(pt2.products[0].code, None)
 
         config = Configuration(1)
         Configuration.write([config], {
-            'product_sequence': product_sequence,
+            'template_sequence': pt_sequence,
             })
 
-        pt3, pt4 = Template.create([{
+        pt3, pt4, pt5 = Template.create([{
                     'name': 'P3',
                     'type': 'goods',
-                    'list_price': Decimal(20),
                     'default_uom': unit.id,
-                    'category_sequence': category1,
+                    'category_sequence': cat1,
                     'products': [('create', [{
                                     'description': 'P3',
                                     }])]
                     }, {
                     'name': 'P4',
                     'type': 'goods',
-                    'list_price': Decimal(20),
                     'default_uom': unit.id,
+                    'category_sequence': cat2,
                     'products': [('create', [{
                                     'description': 'P4',
                                     }])]
+                    }, {
+                    'name': 'P5',
+                    'type': 'goods',
+                    'default_uom': unit.id,
+                    'products': [('create', [{
+                                    'description': 'P5',
+                                    }])]
                     }])
-        self.assertEqual(pt3.products[0].code, 'CAT1')
-        self.assertEqual(pt4.products[0].code, 'PROD1')
+        self.assertEqual(pt3.code, 'CAT-PT1')
+        self.assertEqual(pt3.products[0].suffix_code, 'CAT-PP1')
+        self.assertEqual(pt3.products[0].code, 'CAT-PT1CAT-PP1')
+        self.assertEqual(pt4.code, 'CAT-PT2')
+        self.assertEqual(pt4.products[0].suffix_code, None)
+        self.assertEqual(pt4.products[0].code, 'CAT-PT2')
+        self.assertEqual(pt5.code, 'PT1')
+        self.assertEqual(pt5.products[0].suffix_code, None)
+        self.assertEqual(pt5.products[0].code, 'PT1')
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
